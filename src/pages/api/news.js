7f8dd152188f5806ae7236eed376d9c3325d5757
unknown <rajan23024@gmail.com>
2023-05-20 18:33:46 +0530

    initial commit

---
+++ src/pages/api/news.js
@@ -0,0 +1,20 @@
+export default async function handler(req, res) {
+  const apiRes = await fetch('https://newsapi.org/v2/top-headlines?country=in&apiKey=6c08c057e51646d4b3c14313b53b05ce&pageSize=5', {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  const apiResJson = await apiRes.json();
+
+  const data = (apiResJson?.articles || []).map((news, i) => {
+    return {
+      key: i + 1,
+      description: news.title,
+      image: news.urlToImage,
+      logo: news.urlToImage,
+      video: false,
+    }
+  });
+  res.status(200).json({ data })
+}
