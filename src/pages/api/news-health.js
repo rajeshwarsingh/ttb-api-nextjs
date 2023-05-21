4249771accadabb72c2ca3291b28f64a6077282f
unknown <rajan23024@gmail.com>
2023-05-21 15:53:20 +0530

    added new routes for health intertenment bussiness

---
+++ src/pages/api/news-health.js
@@ -0,0 +1,24 @@
+import config from '../../../config';
+
+export default async function handler(req, res) {
+  const apiRes = await fetch(`https://newsapi.org/v2/top-headlines?category=health&apiKey=${config.newsAPIKey}&pageSize=30`, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  const apiResJson = await apiRes.json();
+
+  const data = (apiResJson?.articles || []).map((news, i) => {
+    return {
+      key: i + 1,
+      description: news.title,
+      image: news.urlToImage,
+      logo: news.urlToImage,
+      sourceLink:news.url,
+      video: false,
+      time: (news?.publishedAt?(new Date(news?.publishedAt)).toLocaleDateString():'')
+    }
+  });
+  res.status(200).json({ data })
+}
